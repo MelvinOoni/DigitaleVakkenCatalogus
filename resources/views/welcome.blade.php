2a582c5fd5f0cfedf00b5553c7d0e08e4ce6636f
melvinooni <melvinoonincx2@gmail.com>
2019-06-19 22:19:33 +0200

    home screen changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,8 +20,19 @@
 @stop
 
 @section('body')
-    <h1>Dashboard</h1>
-    <p>Welcome to this awesome web app!</p>
+    <h1>Realisatie:</h1>
+    <div class="card mb-3">
+        <img src="/images/realisatie.png" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">Overzicht per blok over jaar 1</h5>
+            <p class="card-text">Een deel van onze realisatie is het maken van een overzicht zoals hierboven.
+                Hierbij is te zien waarmee de 1e jaars mee te maken gaan hebben.
+                Hier is onder andere in te zien: welk vak, welke periode voor welke toets ze hiermee te maken krijgen.
+                In dit overzicht is ook te zien hoe je hiervoor getoetst wordt.
+                Wij willlen hier nog aan toevoegen dat we kunnen zien hoeveel pogingen je hiervoor krijgt en projectvoorbeelden. </p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
 @endsection
 
 @push('js')
