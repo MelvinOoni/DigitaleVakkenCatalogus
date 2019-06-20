518352a1cadd2eed4b7ac32420d29c36377c601a
williamnijsse <william.nijsse@outlook.com>
2019-06-20 06:50:06 +0200

    Update pdf

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -31,10 +31,26 @@
         <div class="main bg-white">
             <div>
                 <img src="./images/hz.png" height="50px" style="margin-top:3px;margin-left:auto;">
-                <h1 class="text-center mt-3" style="color:#4682B4;">Vakken overzicht</h1>
+                <h1 class="text-center mt-3" style="color:#4682B4;">Compleet overzicht</h1>
             </div>
             <div class="bg-white">
-                <h2 class="mt-3">Courses</h2>
+                <h2 class="mt-3">Blokken</h2>
+                @foreach ($terms as $term)
+                    <div class="list-group-item mt-4">
+                        <div class="card-body">
+                            <h3 class="card-title">{{$term->title}}</h3>
+                            <hr>
+                            <h4>Beschrijving:</h4>
+                            <p> {{$term->description}}</p>
+                            <h4>Semester:</h4>
+                            <p>{{$term->semester}}</p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="breakNow"></div>
+            <div class="bg-white">
+                <h2 class="mt-3">Vakken</h2>
                 @foreach ($courses as $course)
                     <div class="list-group-item mt-4">
                         <div class="card-body">
@@ -48,18 +64,28 @@
                     </div>
                 @endforeach
             </div>
+
             <div class="breakNow"></div>
+
             <div class="bg-white">
-                <h2 class="mt-3">Blokken</h2>
-                @foreach ($terms as $term)
+                <h2 class="mt-3">Toetsen</h2>
+                @foreach ($tests as $test)
                     <div class="list-group-item mt-4">
                         <div class="card-body">
-                            <h3 class="card-title">{{$term->title}}</h3>
+                            <h3 class="card-title">{{$test->course->title}}</h3>
                             <hr>
-                            <h4>Beschrijving:</h4>
-                            <p> {{$term->description}}</p>
-                            <h4>Semester:</h4>
-                            <p>{{$course->semester}}</p>
+                            <h4>Type toets:</h4>
+                            <p> {{$test->type}}</p>
+                            <h4>Aantal pogingen:</h4>
+                            <p>{{$test->attempt}}</p>
+                            <h4>Week:</h4>
+                            <p>{{$test->week}}</p>
+
+
+                            <strong>Vak:</strong> {{ $test->course->title }}<br>
+                            <strong>Type toets:</strong> {{ $test->type }}<br>
+                            <strong>Aantal mogelijke pogingen:</strong> {{ $test->attempt }}<br>
+                            <strong>Week:</strong> {{ $test->week }}<br>
                         </div>
                     </div>
                 @endforeach
